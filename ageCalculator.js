54390947d6eefd9712480e5706cf9ab5d68d90a1
Shuchita <shuchita@live.com>
2020-01-01 07:09:07 +0000

    Age Calculator

---
+++ ageCalculator.js
@@ -0,0 +1,5 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+console.log(ageCalculator("Shuchita", 1994, 2019));
+console.log(ageCalculator("Amita", 1997, 2019));
