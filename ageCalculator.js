a020f3991bfa96b7722a4db702dc6bc35c669720
Shuchita <shuchita@live.com>
2020-01-01 11:29:43 +0000

    More files to add

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,5 +1,3 @@
 function ageCalculator(name, yearOfBirth, currentYear) {
   return name + " is " + (currentYear - yearOfBirth) + " years old.";
 }
-console.log(ageCalculator("Shuchita", 1994, 2019));
-console.log(ageCalculator("Amita", 1997, 2019));
